1f55b8cc55fb78eb6324f93e50a83f6ed6b14b87
EPIscobar <pietrochiodi164@gmail.com>
2026-02-27 11:47:21 -0300

    Tipos de dados em JS

---
+++ inicio/dados2.js
@@ -0,0 +1,14 @@
+// console.log(19.9 * 0.6);
+
+var preco = 19.9;
+var desconto = 0.6;
+var precoDesconto = preco * (1 - desconto);
+
+// console.log(precoDesconto);
+
+let nome = "caderno";
+let categoria = "papelaria";
+console.log("produto: " + nome + 
+    ", categoria: " + categoria +
+    ",preco: " + preco + 
+    ", desconto: " + desconto);
---
+++ inicio/dados3.js
@@ -0,0 +1,17 @@
+let idade = 16;
+console.log(typeof idade);
+console.log(typeof 16);
+
+let valor = 1500.50;
+console.log(typeof valor);
+
+let estaChovendo = false;
+console.log(typeof estaChovendo);
+
+let nome = "Everson";
+console.log(typeof nome);
+
+console.log(1 +1);
+console.log("1" + 1);
+
+console.log(valor * "valor");
