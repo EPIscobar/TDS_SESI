1fad1357d9c2e7df027d5c928c7e3dd6dfaee6e5
EPIscobar <pietrochiodi164@gmail.com>
2026-03-06 11:39:47 -0300

    operadores de atribuição e relacionais

---
+++ operadores/desafio_relacionais.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 11;
+
+// imprimir no console 5 num true
+console.log(a < b);
+console.log(a != b);
+console.log(a <= b);
+console.log(b !== a);
+console.log(b >! a);
+
+console.log("\n"); //deixar mais bonito
+
+// imprimir no console 5 num false
+console.log(a > b);
+console.log(a == b);
+console.log(a >= b);
+console.log(a === b);
+console.log(b <! a);
---
+++ operadores/relacionais2.js
@@ -0,0 +1,16 @@
+let a =1; //number
+let b = "1"; //string
+let c = "1"; //string
+
+// = atribuição
+// == compara somente valor
+// === compara valor e tipo
+
+console.log(a == b);
+console.log(a === b);
+console.log(b === a);
+
+ console.log(a != b); //false
+ console.log(a !== b); //true
+ console.log(b !== c); //false
+
