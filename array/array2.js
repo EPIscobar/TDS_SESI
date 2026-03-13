cee887b06bba699de32de9f41963d6ccb0fd1154
EPIscobar <pietrochiodi164@gmail.com>
2026-03-13 12:18:19 -0300

    arrays.js

---
+++ array/array2.js
@@ -0,0 +1,10 @@
+// Desafio: Inserir 5 novos índices com 5 nomes e imprimir na tela
+
+const nomes = [];
+nomes[0] = "zoio";
+nomes[1] = "alek";
+nomes[2] = "predo";
+nomes[3] = "felipão";
+nomes[4] = "miguelito";
+console.log(nomes);
+console.log("\n" + nomes.length); 
---
+++ array/desafio_array.js
@@ -0,0 +1,15 @@
+const numeros = [];
+
+// Inserir 10 numeros utilizando a função .push ( ) Iprimir na tela utilizando o fomato: "numero: X"
+
+numeros.push(10);
+numeros.push(20)
+numeros.push(30);
+numeros.push(40);
+numeros.push(50);
+numeros.push(60);
+numeros.push(70);
+numeros.push(80);
+numeros.push(90);
+numeros.push(100);
+
