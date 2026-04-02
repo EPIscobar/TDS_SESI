297a1bd9439822e5854fd4941acfe709557b8f00
EPIscobar <pietrochiodi164@gmail.com>
2026-04-02 12:17:00 -0300

    desafios.js

---
+++ atividades/desafio2.js
@@ -0,0 +1,9 @@
+let idadeDog = 5;
+
+if(idadeDog < 1){
+console.log("digite um numero maior que 0");
+}
+idadeCalculada = idadeDog * 7;
+console.log(`a idade do seu cachorrro é ${idadeCalculada}
+    pq 1 ano para ser humano equivale a 7 para seu cachorro`);
+    
